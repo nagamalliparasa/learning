675c7cd23719cab9fd24d0515d454480e45fa6d4
nagamalliparasa <nagamallirgukt@gmail.com>
2026-08-15 10:50:30 +0530

    Updated datatypes

--- c++/basic.cpp
+++ c++/basic.cpp
@@ -5,5 +5,13 @@ int main(){
     int a = 10; 
     int b = 20; 
     cout<<a<<" "<<b<<endl;
-    return 0; 
+    // return 0; 
+
+    // data types are added
+    /* Here are the datatypes that are mostly in usage
+    */
+   int a=10;
+   int b=20;
+   cout<<"a+b="<<(a+b)<<endl;
+   return 0;
 }
